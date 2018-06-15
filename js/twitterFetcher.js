bb9f01ed0fe11f169fcccd3d8c07f9c263350a6d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-06-15 17:23:03 -0400

    Adding the noopener to the links generated by twitterFetcher.js file.

--- js/twitterFetcher.js
+++ js/twitterFetcher.js
@@ -374,10 +374,10 @@
             op += '<p class="interact"><a href="https://twitter.com/intent/' +
                 'tweet?in_reply_to=' + tids[n] +
                 '" class="twitter_reply_icon"' +
-                (targetBlank ? ' target="_blank">' : '>') +
+                (targetBlank ? ' target="_blank" rel="noopener">' : '>') +
                 'Reply</a><a href="https://twitter.com/intent/retweet?' +
                 'tweet_id=' + tids[n] + '" class="twitter_retweet_icon"' +
-                (targetBlank ? ' target="_blank">' : '>') + 'Retweet</a>' +
+                (targetBlank ? ' target="_blank" rel="noopener">' : '>') + 'Retweet</a>' +
                 '<a href="https://twitter.com/intent/favorite?tweet_id=' +
                 tids[n] + '" class="twitter_fav_icon"' +
                 (targetBlank ? ' target="_blank">' : '>') + 'Favorite</a></p>';
